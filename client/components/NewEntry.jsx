1a3dd15e5bf9b1af703bc15b62e69e52210df06c
zozo-url <zozo-url@protonmail.ch>
2019-06-07 18:19:19 +1200

    add tags feature in NewEntry component

--- client/components/NewEntry.jsx
+++ client/components/NewEntry.jsx
@@ -6,62 +6,76 @@ import data from '../../data/data.json'
 class newEntry extends React.Component {
     constructor(props) {
         super(props)
-
         this.state = {
             "date": '',
             "mood": '',
             "external": '',
-            "reflection": ''
+            "reflection": '',
+            "tags": [],
         }  
         this.setMood = this.setMood.bind(this)
         this.setFactor = this.setFactor.bind(this)
         this.saveEntry = this.saveEntry.bind(this)
+        this.setTags = this.setTags.bind(this)
     }
     setMood(e) {
         let text = e.target.innerHTML
         this.setState(state => ({
             mood: text
         }))
-        console.log(this.state)
     }
     setFactor(e) {
         let text = this.state.external + '  ' + e.target.innerHTML
         this.setState(state => ({
             external: text
         }))
-        // this.state.external.push(text)
-        console.log(this.state)
+    }
+    setTags(e) {
+        let tagInput = document.getElementById('tag');
+        let newTag = tagInput.value;
+       let newTagArray = this.state.tags
+       newTagArray.push(newTag)
+        this.setState(state => ({
+            tags: newTagArray
+        }))
+        tagInput.value = '';
     }
     saveEntry(e) {
-        
         this.state.reflection = document.getElementById('journal').value
         this.state.date = new Date()
-        console.log(this.state)
         var entry = this.state
         postEntry(entry)
-
-        // postEntry(this.state)
-        //set this data to something, send it to database
-        //go back to homepage/ to view entries
     }
+
 render () {
   return (
     <div>
         <h1>How are you?</h1>
-            <div className = 'options'>
+        <div className = 'options'>
             {data.moods.map(mood => {
-            return <p className='button' key={mood} onClick={this.setMood}>{mood}</p>
+                return <p className='button' key={mood} onClick={this.setMood}>{mood}</p>
             })}
-            </div>
+        </div>
       <br/>
-         <h1>External factors</h1>
-           <div className = 'options'>
+        <h1>External factors</h1>
+        <div className = 'options'>
            {data.extFactor.map(factor => {
-            return <p className='button' key={factor} onClick={this.setFactor}>{factor}</p>
+                return <p className='button' key={factor} onClick={this.setFactor}>{factor}</p>
             })}
-            </div>
+        </div>
         <h1>Detail your experience</h1>
-        <div className = 'bottomOfForm'><textarea rows='10' cols='100' id='journal'/> <br/> <br/>
+        <div className = 'bottomOfForm'><textarea rows='10' cols='100' id='journal'/>
+        <br/>
+        <h1>Add tags</h1>
+        {this.state.tags.map((tag, i) => {
+            return <p key={i}>{tag}</p>
+        })}
+        <div className = 'tags'>
+            <input type='text' id='tag'></input> 
+            <h3 className='submit' onClick={this.setTags}>add</h3>
+            <br/>
+        </div>
+        <br/>
         <Link to='/view'><h1 className='submit' onClick={this.saveEntry}>submit</h1></Link></div>
     </div>
 )  
